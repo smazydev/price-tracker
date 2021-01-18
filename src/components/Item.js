f5643a39d94f13636ebabd165f8de5356f335ece
smazydev <alizafar1437@gmail.com>
2021-01-18 18:53:28 +0500

    added secondary-nav styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ import ProductOne from '../static/images/product-1.png';
 
 const Item = () => {
     return (
-        <div>
+        <div className="item-container">
             <img src={ProductOne} alt="Macbook"/>
             <h1>apple macBook pro 13' 2.4 <br/> ghz - 8 GB RAM</h1>
             <div className="price-container">
--- src/components/SecondaryNav.js
+++ src/components/SecondaryNav.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Heart from '../static/images/heart.png';
 
 const SecondaryNav = () => {
     return (
         <div className="nav-container no-border">
             <select className="item-sorter">
-                <option label="best prices"></option>
-                <option label="trending items"></option>
-                <option label="surprise me"></option>
+                <option label="Best Prices">best prices</option>
+                <option label="Trending Items">trending items</option>
+                <option label="Surprise Me">surprise me</option>
             </select>
             <ul className="nav-list">
                 <a className="nav-link" href="Home"><li>amazon us</li></a>
@@ -14,7 +15,7 @@ const SecondaryNav = () => {
                 <a className="nav-link" href="Home"><li>amazon canada</li></a>
                 <a className="nav-link" href="Home"><li>amazon india</li></a>
             </ul>
-            <button className="signup">sign up</button>
+            <button className="favorite"><img src={Heart} alt="favorite button"/></button>
         </div>
     )
 }
